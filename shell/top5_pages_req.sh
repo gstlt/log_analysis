ec967a41422b433172dd589561ae2df305b8aa22
Grzegorz Adamowicz <grzegorz.adamowicz@kaufda.de>
2016-01-05 17:02:20 +0100

    Fixing top5 uris for top 10 ips script

--- shell/top5_pages_req.sh
+++ shell/top5_pages_req.sh
@@ -10,7 +10,7 @@ awk '{num_reqs[$1]++}END{ for (ip in num_reqs) print num_reqs[ip] " " ip}' ${fil
 while read line
 do
   echo "For IP ${line}"
-  grep "${line}" $filename | awk '{print $7}' access.log | sort | uniq -c | sort -rn | head -5
+  grep "^${line}" $filename | awk '{print $7}' | sort | uniq -c | sort -rn | head -5
   echo ""
 done <${tmp_file}
 
